8a91e97b8ba170c018ec23e59e6d98679a672e95
21343048 <112170418+21343048@users.noreply.github.com>
2022-10-15 20:25:23 +0700

    Add files via upload

---
+++ js8/src/jobsheet8/ArraySample.java
@@ -0,0 +1,15 @@
+package jobsheet8;
+
+/**
+ *
+ * Created by Ghenta Alif Alde_21343048
+ */
+
+public class ArraySample {
+    public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i <100; i++){
+            System.out.print(ages[i]);
+        }
+    }
+}
---
+++ js8/src/jobsheet8/ArraySample2.java
@@ -0,0 +1,15 @@
+package jobsheet8;
+
+/**
+ *
+ * Created by Ghenta Alif Alde_21343048
+ */
+
+public class ArraySample2 {
+    public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i < ages.length; i++){
+            System.out.print(ages[i]);
+        }
+    }
+}
